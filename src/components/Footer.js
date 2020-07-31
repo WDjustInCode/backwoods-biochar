fc9e80a9553dba4eab5b64623145e3b42dd67e15
WDjustInCode <justinblocker82@gmail.com>
2020-07-31 16:13:30 -0700

    updated product/footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,27 +10,11 @@ import { FaPhone } from "react-icons/fa/";
 const Footer = class extends React.Component {
   render() {
     return (
-      <footer id="footer" className="footer has-text-white-ter pt-0 full-width-image-container margin-top-0" style={{width: '100%', backgroundImage: `url('/img/home-jumbotron.jpg')`,
+      <footer id="footer" className="footer has-text-white-ter margin-top-0" style={{width: '100%', backgroundImage: `url('/img/home-jumbotron.jpg')`,
       backgroundPosition: `bottom center`,
-      backgroundAttachment: `fixed`, marginBottom: 0}}>
-        <div className="columns has-text-white-ter" style={{margin: 0}}>
-          <div className="column is-4 has-text-centered">
-            <div className="columns is-mobile is-multiline is-centered" style={{height: '100%'}}>
-              <div className="column is-7 py-3">
-                <p className="is-size-4" style={{backgroundColor: 'rgb(86, 121, 91)'}}>Contact Us:</p>
-              </div>
-              <section className="column is-full py-3" >
-                <IconContext.Provider value={{ color: "white", size: 35, className: "" }}>
-                  <div className="">
-                      <a href="tel:+1360-241-2244" className="px-4"><span><FaPhone /></span></a>
-                      <a href="mailto:joshua@backwoodsbiochar.com" className="px-4"><span><FaEnvelope /></span></a>
-                      <a href="https://www.facebook.com/Backwoods-Biochar-104956791259951" className="px-4"><span><FaFacebook /></span></a>
-                      <a href="https://www.instagram.com/backwoods_biochar_llc/" className="px-4"><span><FaInstagram /></span></a>
-                  </div>
-                </IconContext.Provider>
-              </section>
-            </div>
-          </div>
+      backgroundAttachment: `fixed`, marginBottom: 0, paddingTop: 25}}
+      >
+        <div className="columns has-text-white-ter" style={{marginBottom: 0, paddingBottom: 25}}>
           <div className="column is-4" style={{padding: 0}}>
             <section className="menu">
               <ul className="menu-list has-text-centered ml-0 mt-0">
@@ -62,6 +46,20 @@ const Footer = class extends React.Component {
                 </li>
               </ul>
             </section>
+          </div>
+          <div className="column is-4 has-text-centered">
+            <div className="columns is-mobile is-multiline is-centered" style={{height: '100%'}}>
+              <section className="column is-full py-3" >
+                <IconContext.Provider value={{ color: "white", size: 25, className: "" }}>
+                  <div className="">
+                      <a href="tel:+1360-241-2244" className="px-4"><span><FaPhone /></span></a>
+                      <a href="mailto:joshua@backwoodsbiochar.com" className="px-4"><span><FaEnvelope /></span></a>
+                      <a href="https://www.facebook.com/Backwoods-Biochar-104956791259951" className="px-4"><span><FaFacebook /></span></a>
+                      <a href="https://www.instagram.com/backwoods_biochar_llc/" className="px-4"><span><FaInstagram /></span></a>
+                  </div>
+                </IconContext.Provider>
+              </section>
+            </div>
           </div>
         </div>
       </footer>
--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
+import { Link ,graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Features from '../components/Features'
 import Testimonials from '../components/Testimonials'
@@ -53,15 +53,6 @@ export const ProductPageTemplate = ({
           </div>
           <div className="columns">
             <div className="column is-10 is-offset-1">
-              <Features gridItems={intro.blurbs} />
-              <div className="columns">
-                <div className="column is-7">
-                  <h3 className="has-text-weight-semibold is-size-3">
-                    {main.heading}
-                  </h3>
-                  <p>{main.description}</p>
-                </div>
-              </div>
               <div className="tile is-ancestor">
                 <div className="tile is-vertical">
                   <div className="tile">
@@ -99,6 +90,13 @@ export const ProductPageTemplate = ({
               </h2>
               <p className="is-size-5">{pricing.description}</p>
               <Pricing data={pricing.plans} />
+            </div>
+            <div className="columns">
+              <div className="column is-12 has-text-centered">
+                <Link className="btn" to="/contact">
+                  Contact US
+                </Link>
+              </div>
             </div>
           </div>
         </div>
